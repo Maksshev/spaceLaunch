592772089dbfb234f7f0e7cad77faf9f1c730e5a
Maksym Shevchenko <maksym.shevchenko@icloud.com>
2018-11-06 13:26:52 +0200

    Scroll

--- src/js/sync.js
+++ src/js/sync.js
@@ -1,12 +1,10 @@
 const $ = require('jquery');
+const moment = require('moment');
 const slick = require('slick-carousel');
 const jQueryBridget = require('jquery-bridget');
 const Masonry = require('masonry-layout');
 let source = `https://launchlibrary.net/1.4/launch/next/20`;
-jQueryBridget( 'masonry', Masonry, $ );
-
-
-
+jQueryBridget('masonry', Masonry, $);
 
 
 async function getInfo() {
@@ -19,6 +17,7 @@ async function getInfo() {
             let mission = json.launches[i].missions[0].description;
             let rocketID = json.launches[i].rocket.id;
             let rocketImgUrl = `https://launchlibrary.net/1.4/rocket/${rocketID}`;
+            let start = moment(json.launches[i].isostart).valueOf();
             let imgRequest = await fetch(rocketImgUrl);
             let jsonImg = await imgRequest.json();
             console.log(jsonImg);
@@ -28,13 +27,15 @@ async function getInfo() {
                 obj: json,
                 mission: mission,
                 rocketImg: rocketID,
-                img: imgUrl
+                img: imgUrl,
+                start: start
             };
             result.push(workingObj);
         } catch (e) {
             continue;
         }
     }
+    console.log(result);
     return result;
 }
 
@@ -47,18 +48,25 @@ function createArticle(callback) {
                 if (!results[i].img.includes('placeholder')) {
                     $('.gallery-container').append(`
                 <div id="${i}" class="gallery-item grid-item">
-                    
-                    <p>${results[i].name}</p>
+                    <img class="rocket-img" src="${results[i].img}" alt="">
+                   
+                    <div class="timer ${i}">
+                        <div class="info-container">
+                        <span class="name">${results[i].name}</span>
+                        <span class="countdown"></span>
+                        <span class="mission">${results[i].mission}</span>
+                        </div>
+                    </div>                    
                 </div>
                 `);
 
                     $(`#${i}`)
                         .css({
-                            backgroundImage: `url('${results[i].img}')`,
                             backgroundSize: 'cover',
                             backgroundPosition: 'center',
 
                         });
+                    createTimer(results[i].start, i);
                 }
             } else {
                 console.log('done');
@@ -73,37 +81,36 @@ function createArticle(callback) {
 createArticle(function () {
     const $grid = $('.grid').masonry({
         itemSelector: '.grid-item',
-        columnWidth: 60,
-        fitWidth: true
+        // columnWidth: 300,
+        fitWidth: true,
     });
 
-    $grid.on( 'click', '.grid-item', function() {
-        // change size of item via class
-        $( this ).toggleClass('grid-item--gigante');
-        // trigger layout
+    $grid.on('click', '.grid-item', function () {
+        $(this).toggleClass('grid-item--gigante');
         $grid.masonry();
+        if ($(this).width() > 250 || $(this).height() > 250) {
+            $('html, body').animate({
+                scrollTop: $(this).offset().top
+            }, 1000);
+        }
     });
 });
 
-// createArticle(function () {$(document).ready(function(){
-//     $('.gallery-container').slick({
-//         slidesToShow: 1,
-//         arrows: true,
-//         prevArrow: `<div class="arrow-left"></div>`,
-//         nextArrow: `<div class='arrow-right'></div>`
-//     });
-// })});
 
-
-
-
-
-
-// function () {$(document).ready(function(){
-//     $('.gallery-container').slick({
-//         slidesToShow: 3,
-//         dots:true,
-//         centerMode: true,
-//     });
-// })}
+function createTimer(startDate, i) {
+    let interval = setInterval(function () {
+        const now = new Date().getTime();
+        if (now <= 0) {
+            clearInterval(interval);
+        }
+        const distance = startDate - now;
+        const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        $(`.${i} .countdown`).animate({opacity: '0.3'}, 250, function () {
+            $(this).text(`${days}d ${hours}h ${minutes}m ${seconds}s`).animate({opacity: '1'}, 250)
+        })
+    }, 1000);
+}
 
